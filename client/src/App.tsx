59adbafbc87da78311872a71d3fb4654486bed54
Hyna <bylo@duck.com>
2025-05-11 20:26:24 +0200

    feat(deploy): test deploy ok

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -30,7 +30,7 @@ function App() {
     <div>
       <Header>
         <Container>
-          <h1>Test Deploy CD/3</h1>
+          <h1>Test Deploy CD</h1>
         </Container>
       </Header>
       <Container>
