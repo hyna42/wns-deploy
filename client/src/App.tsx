0fda5ed39e79b0b7781ff5e989dd59c903e6f0a5
hyna <bylo@duck.com>
2025-05-17 00:07:33 +0200

    feat(cd): last test deploy full ci/cd

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -30,7 +30,7 @@ function App() {
     <div>
       <Header>
         <Container>
-          <h1>Test Deploy CD : SHA ok </h1>
+          <h1>Test Deploy CI/CD </h1>
         </Container>
       </Header>
       <Container>
